c2ea800889a65d46690363a0f2ba5852fce4cffb
Jasmine <jazzyysuarez@gmail.com>
2025-09-04 21:41:04 -0700

    struct shit

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
-int main() {
-    cout << "Hello World!" << endl;
-    cout << "Testing" << endl;
+struct Part
+{
+    int quantity;
+    string partNr;
+    double density;
+    int price;
+};
+
+int main()
+{
+    vector<Part> storeParts;
+
+    Part temp;
+    temp.quantity = 15;
+    temp.partNr = "IWX";
+    temp.density = 14.4;
+    temp.price = 40;
+    storeParts.push_back(temp);
 }
